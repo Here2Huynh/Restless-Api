b84f83506d989ba61d231e96147da1261572bfa1
John Huynh <jhnhnyh9@gmail.com>
2018-11-11 19:54:41 -0800

    implemented routes for post, pust, delete and schema validation

--- app.js
+++ app.js
@@ -1,18 +1,89 @@
 
+const Joi = require('joi')
 const express = require('express')
-
 const app = express()
+
+// adding middleware to process the req body
+app.use(express.json())
+
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+]
 
 app.get('/', (req, res) => {
     res.send('Hello world!!!!!!')
 })
 
 app.get('/api/courses', (req , res) => {
-    res.send([9,3,2,3,41,5,1,21,4,512,3,123,1222])
+    res.send(courses)
 })
 
+// POST
+app.post('/api/courses', (req,res) => {
+    const { error } = validateCourse(req.body)
+
+    if ( error ) return res.status(400).send(error.details[0].message)
+ 
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
+
+// PUT = updating resources
+app.put('/api/courses/:id', (req, res) => {
+    // look up the course
+    // if it doesn't exist, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if ( !course ) return res.status(404).send('The course with the given ID was not found.')
+ 
+    // validate
+    // if invalid, return 400 - Bad request
+    // const result = validateCourse(req.body)
+    const { error } = validateCourse(req.body) // result.error 
+
+    if ( error ) return res.status(400).send(error.details[0].message)
+  
+
+    // update course 
+    // return the updated course
+    course.name = req.body.name
+    res.send(course)
+})
+
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    return Joi.validate(course, schema)
+}
+
+app.delete('/api/courses/:id', (req,res) => {
+    // look up the course
+    // if doesn't exist, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if ( !course ) return res.status(404).send('The course with the given ID was not found.')
+
+    // delete
+    const index = courses.indexOf(course)
+    courses.splice(index,1)
+
+    // return the same course
+    res.send(course)
+})
+
+
 app.get('/api/courses/:id', (req , res) => {
-    res.send(req.params.id)
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send('The course with the given ID was not found.')
+    res.send(course)
 })
 
 app.get('/api/posts/:year/:month', (req , res) => {
