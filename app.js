deb0df23891688a23a4b0010e3d8810faabd85d9
John Huynh <jhnhnyh9@gmail.com>
2018-11-13 22:57:20 -0800

    implemented debug package, refactored  the routes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
+const debug = require('debug')('app:startup')
 const config = require('config')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const Joi = require('joi')
-const logger = require('./logger')
-const authenicater = require('./authenticator')
+const logger = require('./middleware/logger')
+const authenicater = require('./middleware/authenticator')
+const courses = require('./routes/courses')
+const homePage = require('./routes/homePage')
 
 const express = require('express')
 const app = express()
@@ -12,7 +15,7 @@ const app = express()
 // configuration
 console.log('Application Name: ' + config.get('name'))
 console.log('Mail Server: ' + config.get('mail.host'))
-console.log('Mail Password: ' + config.get('mail.password'))
+// console.log('Mail Password: ' + config.get('mail.password'))
 
 // console.log(`NODE_ENV: ${process.env.NODE_ENV}`) //return environment of the app, if not set it will be undefined
 
@@ -20,8 +23,12 @@ console.log('Mail Password: ' + config.get('mail.password'))
 
 if ( app.get('env') === 'development' ) {
     app.use(morgan('tiny'))
-    console.log('Morgan enabled...')
+    debug('Morgan enabled...')  // more insighful console.log()
 }
+
+// templating engine, usually use if we decide to return a view for the web service 
+app.set('view engine', 'pug')
+app.set('views', './views') // default //option setting
 
 
 // adding middleware to process the req body
@@ -29,97 +36,16 @@ app.use(express.json()) // parses req.body
 app.use(express.urlencoded({ extended: true })) // key=value&key=value
 app.use(express.static('public'))
 app.use(helmet())
+app.use('/api/courses', courses) // any endpoint using that endpoint, use that router
+app.use('/', homePage)
 
 
+// use middleware
 app.use(logger)
-
 app.use(authenicater)
 
-const courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'},
-]
-
-app.get('/', (req, res) => {
-    res.send('Hello world!!!!!!')
-})
-
-app.get('/api/courses', (req , res) => {
-    res.send(courses)
-})
-
-// POST
-app.post('/api/courses', (req,res) => {
-    const { error } = validateCourse(req.body)
-
-    if ( error ) return res.status(400).send(error.details[0].message)
- 
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course)
-    res.send(course)
-})
-
-// PUT = updating resources
-app.put('/api/courses/:id', (req, res) => {
-    // look up the course
-    // if it doesn't exist, return 404
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if ( !course ) return res.status(404).send('The course with the given ID was not found.')
- 
-    // validate
-    // if invalid, return 400 - Bad request
-    // const result = validateCourse(req.body)
-    const { error } = validateCourse(req.body) // result.error 
-
-    if ( error ) return res.status(400).send(error.details[0].message)
-  
-
-    // update course 
-    // return the updated course
-    course.name = req.body.name
-    res.send(course)
-})
 
 
-function validateCourse(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
-
-    return Joi.validate(course, schema)
-}
-
-app.delete('/api/courses/:id', (req,res) => {
-    // look up the course
-    // if doesn't exist, return 404
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if ( !course ) return res.status(404).send('The course with the given ID was not found.')
-
-    // delete
-    const index = courses.indexOf(course)
-    courses.splice(index,1)
-
-    // return the same course
-    res.send(course)
-})
-
-
-app.get('/api/courses/:id', (req , res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send('The course with the given ID was not found.')
-    res.send(course)
-})
-
-app.get('/api/posts/:year/:month', (req , res) => {
-    res.send(req.query)
-    // params => '/:value'
-    // query => '?query'
-})
 
 // PORT
 const port = process.env.PORT || 3000
