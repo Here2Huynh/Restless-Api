5dfaf6ed0bb54d5d30dc8739084752e2b2fc64aa
John Huynh <jhnhnyh9@gmail.com>
2018-11-16 20:31:34 -0800

    practiced async and await

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,26 +1,46 @@
 console.log('Before')
 // const user = getUser(1, getRepositories)
-console.log('After')
 
-function displayRepo(user) {
-    getRepositories(user.gitHubUsername, displayCommits)
-}
 
-function displayCommits(repo) {
-    console.log(repo)
-    getCommits(repo, displayCommits)
-}
-
-function displayCommits(commits) {
+// async and await, just syntaxical sugar, but just Promises that looks synchronously
+// when using await, need to decorate with async
+async function displayCommits() {
+    try {
+        const user = await getUser(1)
+        const repos = await getRepositories(user.gitHubUsername)
+        const commits = await getCommits(repos[0])
+    }
+    catch (err) {
+        console.log('Error', err.message)
+    }
     console.log(commits)
 }
+displayCommits()
 
-// promises
-getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos))
-    .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('Error', err.message))
+
+console.log('After')
+
+// callback approach using named functions
+// function displayRepo(user) {
+//     getRepositories(user.gitHubUsername, displayCommits)
+// }
+
+// function displayCommits(repo) {
+//     console.log(repo)
+//     getCommits(repo, displayCommits)
+// }
+
+// function displayCommits(commits) {
+//     console.log(commits)
+// }
+
+// promises approach 
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err => console.log('Error', err.message))
+
 
 
 function getUser(id) {
@@ -38,6 +58,7 @@ function getRepositories(username) {
         setTimeout( () => {
             console.log(`Reading ${username}'s repositories...`)
             resolve([ 'repo1', 'repo2', 'repo3' ])
+            // reject(new Error('Could not get the repos.'))
         }, 2000)
     })
 }
--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -3,7 +3,34 @@
 // const p = Promise.resolve({ id: 1 })
 // p.then(result => console.log(result))
 
-const p = Promise.reject(new Error('reason for rejection...'))
-p.catch(err => console.log(err))
+// const p = Promise.reject(new Error('reason for rejection...'))
+// p.catch(err => console.log(err))
 
 // better to use native error obj because it returns the callstack
+
+
+// parellel promises
+// thread kicks off the operations in order and then goes into the next one
+const p1 = new Promise( (resolve, reject) => {
+    setTimeout( () => {
+        console.log('Async operation 1...')
+        // reject(new Error('something done goofed...'))
+        resolve(1)
+    }, 2000)
+})
+
+const p2 = new Promise( (resolve) => {
+    setTimeout( () => {
+        console.log('Async operation 2...')
+        resolve(2)
+    }, 5000)
+})
+
+// Promise.all([p1,p2])
+//     .then(result => console.log(result))  // will be available as an array
+//     .catch(error => console.log(error.message)) // if any error, the whole promise.all is rejected
+
+// when you don't want to wait for all to complete
+Promise.race([p1,p2])
+    .then(result => console.log(result))  
+    .catch(error => console.log(error.message)) 
