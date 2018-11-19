d7769c22f27a7a6c67ae446985041ab7204c9d01
John Huynh <jhnhnyh9@gmail.com>
2018-11-18 22:38:35 -0800

    practiced mongo queries, updating and deleting docs

--- mongo-demo/exercise/index.js
+++ mongo-demo/exercise/index.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 // connect to db
-mongoose.connect('mongodb://localhost/mongo-exercises')
+mongoose.connect('mongodb://localhost/mongo-exercises', { useNewUrlParser: true })
     .then(() => console.log('Connected to mongo-exercises db...'))
     .catch(error => console.log('Fail to connect to db...', error))
 
@@ -21,15 +21,41 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model('Course', courseSchema)
 
 // create query function 
-async function getCourses() {
-    return await Course
-    .find({ isPublished: true, tags: 'backend' })
-    .sort( { name: 1 } )   // or .sort('name') or .sort('-name')
-    .select( { name: 1, author: 1 })  // or .select('name author')
+// async function getCourses() {
+//     return await Course
+//     .find({ isPublished: true, tags: 'backend' })
+//     .sort( { name: 1 } )   // or .sort('name') or .sort('-name')
+//     .select( { name: 1, author: 1 })  // or .select('name author')
+// }
+
+// async function getCourses2() {
+//     return await Course 
+//         // .find({ isPublished: true , tags: { $in: [ 'frontend', 'backend' ] } })
+//         .find({ isPublished: true })
+//         .or([ { tags: 'frontend'}, { tags: 'backend' }])
+//         .sort({ price: -1 })
+//         .select({ name: 1, author: 1, price: 1 })
+// }
+
+async function getCourses3() {
+    return await Course 
+        .find({ isPublished: true })
+        .or([ 
+            { price: { $gte: 15 } }, 
+            { name: /.*by*./i }
+        ])
+        .sort('-price')
+        .select('name author price')
 }
 
 async function run() {
-    const courses = await getCourses() 
-    console.log(courses)
+    // const courses = await getCourses() 
+    // console.log(courses)
+
+    // const courses2 = await getCourses2()
+    // console.log(courses2)
+
+    const course3 = await getCourses3()
+    console.log(course3)
 }
 run()
---
+++ mongo-demo/index.js
@@ -0,0 +1,131 @@
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err))
+
+// schema is specific to mongoose
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+})
+
+// objs are an instance of the class, class is a blueprint for the obj 
+
+const Course = mongoose.model('Course', courseSchema) // class 
+
+// create document
+async function createCourse() {
+    const course = new Course({
+        name: 'React.js Course',
+        author: 'Mosh',
+        tags: [ 'react', 'frontend' ],
+        isPublished: true
+    }) // obj
+    
+    
+    const result = await course.save() // async op, because it accessing the fs and db 
+    console.log(result)
+}
+
+// querying documents 
+async function getCourses() {
+    // comparison operators 
+    // eq ===
+    // ne !==
+    // gt >
+    // gte >=
+    // lt <
+    // lte <=
+    // in
+    // nin (not in)
+
+    // logical operators
+    // or 
+    // and 
+
+    const courses = await Course
+        // comparison operators 
+        // .find({ price: { $gt: 10, $lte: 20 } })   // to express comparison, we need to pass objs with $
+        // .find({ price: { $in: [ 10, 15, 20 ] }})
+
+        // logical operators 
+        // .find()
+        // .or([ { author: 'Mosh' }, { isPublished: true } ])
+        // .and([ { author: 'Mosh' }, { isPublished: true } ])
+
+        // regular expression
+        // starts with Mosh
+        // .find({ author: /^Mosh/ })
+        // ends with Hamedani
+        // .find({ author: /Hamedani$/i })  // by default it will be case sensitive, i = case INsensitive 
+
+        // contains Mosh 
+        // .find({ author: /.*Mosh*/i }) // wildcards
+
+        // const pageNumber = 2
+        // const pageSize = 10
+        // // /api/courses?pageNumber=2&pageSize=10
+        // .skip((pageNumber - 1 ) * pageSize) //pagination 
+        // .limit(pageSize)
+
+        .find({ author: 'Mosh', isPublished: true })  // filtering 
+        .limit(10)
+        .sort({ name: 1 })  // 1 is ascending, -1 is descending 
+        // .select({ name: 1, tags: 1 })  // select props to be returned 
+        .count() 
+    console.log(courses)
+}
+
+// getCourses()
+
+async function updateCourse(id) {
+    // approach: query first
+    // findById()
+    // modify its properties
+    // save()
+    // const course = await Course.findById(id)
+    // if (!course) return;
+    // course.isPublished = true
+    // course.author = 'Another author'
+    // // course.setOptions({
+    // //     isPublished: true,
+    // //     author: 'Another author'
+    // // })
+    // const result = await course.save()
+    // console.log(result)
+
+    // approach: update first
+    // update directly
+    // optionally: get the updated document 
+    // const result = await Course.update({ _id: id}, {
+    //     $set: {
+    //         author: 'Mosh',
+    //         isPublished: false
+    //     }
+    // })
+    // console.log(result)
+
+    // if you want to get the doc that was updated
+    const result = await Course.findByIdAndUpdate(id, {
+        $set: {
+            author: 'Jason',
+            isPublished: false
+        }
+    }, { new: true })
+    console.log(result)
+}
+
+// updateCourse('5befbeef4bbe070d76c63bbb')
+
+async function removeCourse(id) {
+    // const result = await Course.deleteOne({ _id: id })  //deleteMany to delete more than one
+    const course = Course.findByIdAndRemove(id)
+    console.log(course)
+}
+removeCourse('5befbeef4bbe070d76c63bbb')
+
