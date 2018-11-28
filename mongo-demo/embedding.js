1b734e8de74099b34a8689d5801ca940add6746b
John Huynh <jhnhnyh9@gmail.com>
2018-11-27 21:50:06 -0800

    implement list of sub docs

--- mongo-demo/embedding.js
+++ mongo-demo/embedding.js
@@ -15,16 +15,13 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author: {
-    type: authorSchema,
-    required: true
-  }
+  authors: [authorSchema]
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
+    authors
   }); 
   
   const result = await course.save();
@@ -57,9 +54,29 @@ async function updateAuthor(courseId) {
   // course.save()
 }
 
-// createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function addAuthor(courseId, author) {
+  const course = await Course.findById(courseId)
+  course.authors.push(author)
+  course.save()
+}
 
-updateAuthor('5bfc9ec3ad8f2c00b169e150')
+async function removeAuthor(courseId, authorId) {
+  const course = await Course.findById(courseId)
+  const author = course.authors.id(authorId)
+  author.remove()
+  course.save()
+}
+
+removeAuthor('5bfe27cb8984e5b82894120a', '5bfe2932233fdbbfc0b843d9')
+
+// addAuthor('5bfe27cb8984e5b82894120a', new Author({ name: 'Amy' }))
+
+// createCourse('Node Course', [
+//   new Author({ name: 'Mosh' })
+//   new Author({ name: 'John' })
+// ])
+
+// updateAuthor('5bfc9ec3ad8f2c00b169e150')
 
 
 // these sub docs can not be saved on their own
